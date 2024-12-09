f162414ac84d7f78f6158c5e30d07950669840bf
balakullayappa chinthapalle <balakullayappa.chinthapalle@serole.com>
2024-12-09 17:06:50 +0530

    Initial commit

--- src/test/java/Registered_Private_car/Private_car.java
+++ src/test/java/Registered_Private_car/Private_car.java
@@ -89,10 +89,11 @@ public class Private_car extends Base_Agent {
 		click("POUDrpDon_XPATH");
 		output("Place Of Use Drop Down Clicked successfully");
 		Thread.sleep(5000);
-		Enter("StateEntr_XPATH", data.get("Place Of Use"));
-		output("state Enterd successfully");
-		Thread.sleep(5000);
-		click("Statesel_XPATH");
+		if(data.get("Place Of Use").equalsIgnoreCase("Johor")) {
+			click("Johor_XPATH");
+		} else if(data.get("Place Of Use").equalsIgnoreCase("Kedah")){
+			click("Kedah_XPATH");
+		} 
 		output("Place Of Use selected successfully");
 		Thread.sleep(5000);
 		click("VeclsrcBtn_XPATH");
@@ -104,10 +105,12 @@ public class Private_car extends Base_Agent {
 		click("CovTpeDrpDon_XPATH");
 		output("Coverage Type Drop Down Clicked successfully");
 		Thread.sleep(5000);
-		Enter("CovrvaleEntr_XPATH", data.get("Coverage Type"));
-		output("Coverage Type Given successfully");
+		if(data.get("Coverage Type").equalsIgnoreCase("Comprehensive")){
+			click("Comprehensive_XPATH");
+		} else if(data.get("Coverage Type").equalsIgnoreCase("Third Party")){
+			click("ThirdParty_XPATH");
+		}
 		Thread.sleep(5000);
-		click("CovrTypSel_XPATH");
 		output("Coverage Type clicked successfully");
 		Thread.sleep(5000);
 		Enter("PHID_XPATH", data.get("PH ID"));
@@ -128,51 +131,18 @@ public class Private_car extends Base_Agent {
 		click("DrvExpDrpDwn_XPATH");
 		output("Driving Experience DropDown clicked successfully");
 		Thread.sleep(5000);
-		Enter("DrvExpGvn_XPATH", data.get("Driving Experience"));
-		output("Driving Experience Given clicked successfully");
+		if(data.get("Driving Experience").equalsIgnoreCase("2 - 5 years")){
+			click("DrvExp2to5y_XPATH");
+		}else if(data.get("Driving Experience").equalsIgnoreCase("6 - 10 years")){
+			click("DrvExp6to10y_XPATH");
+		}
 		Thread.sleep(5000);
-		click("DrvExpDrpDwnSel_XPATH");
 		output("Driving Experience selected successfully");
 
-		// Rebate
+		// Proceed to Policy Issuance
 		Thread.sleep(5000);
-		click("Rebate_id");
-		// PIAM Analysis Code
-		// Garage Type
-		Thread.sleep(5000);
-		click("GrgTypDrpdwn_XPATH");
-		output("Garage Type Drop Down was Click successfully");
-		Thread.sleep(5000);
-		Enter("GrgTypGivn_XPATH", data.get("Garage Type"));
-		output("Garage Type Given successfully");
-		Thread.sleep(5000);
-		click("GrgTypSect_XPATH");
-		output("Garage Type Drop Down was selected successfully");
-		// Anti Theft
-		Thread.sleep(5000);
-		click("AntThtDrpdwn_XPATH");
-		output("Anti Theft Drop Down was Click successfully");
-		Thread.sleep(5000);
-		Enter("AntThtGivn_XPATH", data.get("Anti Theft"));
-		output("Anti Theft Given successfully");
-		Thread.sleep(5000);
-		click("AntThtSect_XPATH");
-		output("Anti Theft Drop Down was selected successfully");
-		// Safety Features
-		Thread.sleep(5000);
-		click("SafFeuDrpdwn_XPATH");
-		output("Safety Features Drop Down was Click successfully");
-		Thread.sleep(5000);
-		Enter("SafFeuGivn_XPATH", data.get("Safety Features"));
-		output("Safety Features Given successfully");
-		Thread.sleep(5000);
-		click("SafFeuSect_XPATH");
-		output("Safety Features Drop Down was selected successfully");
-
+		click("PolicyIssue_XPATH");
 		// Policy Issuance
-		Thread.sleep(15000);
-		click("PolicyBtn_XPATH");
-		output("Proceed to Policy Issuance Button Clicked successfully");
 		Thread.sleep(15000);
 		click("IssPolicy_XPATH");
 		output("Issue Policy Button Clicked successfully");
@@ -182,60 +152,60 @@ public class Private_car extends Base_Agent {
 		Thread.sleep(5000);
 		String QuoteNumber = driver.findElement(By.xpath(loc.getProperty("QuoteNumber_XPATH"))).getText();
 		output(QuoteNumber);
-		write(QuoteNumber, m, 12, rowNum);
+		write(QuoteNumber, m, 9, rowNum);
 		Thread.sleep(5000);
 		String QuoteStatus = driver.findElement(By.xpath(loc.getProperty("QuoteStatus_XPATH"))).getText();
 		output(QuoteStatus);
-		write(QuoteStatus, m, 13, rowNum);
+		write(QuoteStatus, m, 10, rowNum);
 		Thread.sleep(5000);
 		String PloicyNumber = driver.findElement(By.xpath(loc.getProperty("PloicyNumber_XPATH"))).getText();
 		output(PloicyNumber);
-		write(PloicyNumber, m, 14, rowNum);
+		write(PloicyNumber, m, 11, rowNum);
 		Thread.sleep(5000);
 		String VehicleSumInsured = driver.findElement(By.xpath(loc.getProperty("VehicleSumInsured_XPATH"))).getText();
 		output(VehicleSumInsured);
-		write(VehicleSumInsured, m, 15, rowNum);
+		write(VehicleSumInsured, m, 12, rowNum);
 		Thread.sleep(5000);
 		String ActPremium = driver.findElement(By.xpath(loc.getProperty("ActPremium_XPATH"))).getText();
 		output(ActPremium);
-		write(ActPremium, m, 16, rowNum);
+		write(ActPremium, m, 13, rowNum);
 		Thread.sleep(5000);
 		String BasicPremium = driver.findElement(By.xpath(loc.getProperty("BasicPremium_XPATH"))).getText();
 		output(BasicPremium);
-		write(BasicPremium, m, 17, rowNum);
+		write(BasicPremium, m, 14, rowNum);
 		Thread.sleep(5000);
 		String NCD = driver.findElement(By.xpath(loc.getProperty("NCD_XPATH"))).getText();
 		output(NCD);
-		write(NCD, m, 18, rowNum);
+		write(NCD, m, 15, rowNum);
 		Thread.sleep(5000);
 		String PremiumafterNCD = driver.findElement(By.xpath(loc.getProperty("PremiumafterNCD_XPATH"))).getText();
 		output(PremiumafterNCD);
-		write(PremiumafterNCD, m, 19, rowNum);
+		write(PremiumafterNCD, m, 16, rowNum);
 		Thread.sleep(5000);
 		String ExtraBenefitsTotal = driver.findElement(By.xpath(loc.getProperty("ExtraBenefitsTotal_XPATH"))).getText();
 		output(ExtraBenefitsTotal);
-		write(ExtraBenefitsTotal, m, 20, rowNum);
+		write(ExtraBenefitsTotal, m, 17, rowNum);
 		Thread.sleep(5000);
 		String GrossPremium = driver.findElement(By.xpath(loc.getProperty("GrossPremium_XPATH"))).getText();
 		output(GrossPremium);
-		write(GrossPremium, m, 21, rowNum);
+		write(GrossPremium, m, 18, rowNum);
 		Thread.sleep(5000);
 		String Rebate = driver.findElement(By.xpath(loc.getProperty("Rebate_XPATH"))).getText();
 		output(Rebate);
-		write(Rebate, m, 22, rowNum);
+		write(Rebate, m, 19, rowNum);
 		Thread.sleep(5000);
 		String SST = driver.findElement(By.xpath(loc.getProperty("SST_XPATH"))).getText();
 		output(SST);
-		write(SST, m, 23, rowNum);
+		write(SST, m, 20, rowNum);
 		Thread.sleep(5000);
 		String StampDuty = driver.findElement(By.xpath(loc.getProperty("StampDuty_XPATH"))).getText();
 		output(StampDuty);
-		write(StampDuty, m, 24, rowNum);
+		write(StampDuty, m, 21, rowNum);
 		Thread.sleep(5000);
 		String TotalPayablePremium = driver.findElement(By.xpath(loc.getProperty("TotalPayablePremium_XPATH")))
 				.getText();
 		output(TotalPayablePremium);
-		write(TotalPayablePremium, m, 25, rowNum);
+		write(TotalPayablePremium, m, 22, rowNum);
 		rowNum++;
 
 		// Navigating to Home Screen
